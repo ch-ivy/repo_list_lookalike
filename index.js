c9e987d232578efaf6bd87bede9d07dd50332cab
Ivy Ti <chivy1221@gmail.com>
2020-11-14 13:36:58 +0200

    done

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-function api() {
+var repoData;
+var info;
+var repo_list;
+window.onload = async (event) => {
+   await api();
+   info = repoData.data.user;
+   repo_list = repoData.data.user.repositories.nodes;
+   console.log(repo_list);
+
+   fill();
+};
+
+async function api() {
    let api = "https://api.github.com/graphql";
-   fetch(api, {
+   await fetch(api, {
       method: "POST",
       headers: {
          Authorization: "bearer 639c2f9a205dec5502fdbe6627287806dd8067c5",
@@ -8,34 +20,35 @@ function api() {
       },
       body: JSON.stringify({
          query: `{
-  user(login: "ch-ivy") {
-    name
-    bio
-    repositories(first: 5) {
-      nodes {
-        id
-        name
-        updatedAt
-        stargazerCount
-        forkCount
-        languages(first: 2) {
-          nodes {
-            id
-            name
-            color
-          }
-        }
-      }
-    }
-  }
-}`,
+                     user(login: "ch-ivy") {
+                     login
+                     name
+                     bio
+                     avatarUrl
+                     repositories(first: 20) {
+                       nodes {
+                         name
+                         updatedAt
+                         stargazerCount
+                         forkCount
+                         description
+                         languages(first: 1) {
+                           nodes {
+                             name
+                             color
+                           }
+                         }
+                       }
+                     }
+                   }
+                 }`,
       }),
    })
       .then((data) => {
          return data.json();
       })
-      .then((data) => {
-         console.log(data);
+      .then(async (data) => {
+         repoData = await data;
       })
       .catch(function (err) {
          // There was an error
@@ -43,4 +56,107 @@ function api() {
       });
 }
 
-function toggleMenu() {}
+function toggleMenu() {
+   var x = document.getElementById("mobile_menu");
+   x.classList.toggle("hide");
+}
+
+function fill() {
+   const options = {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+   };
+   var img = document.querySelectorAll("img");
+   for (i = 0; i < img.length; i++) {
+      img[i].src = `${info.avatarUrl}`;
+   }
+   var name = document.querySelector(".name");
+   var u_name = document.querySelector(".u-name");
+   var bio = document.querySelector(".bio > p");
+   var repo = document.querySelector(".list-box > ul");
+   name.innerHTML = `${info.name}`;
+   u_name.innerHTML = info.login;
+   bio.innerHTML = info.bio;
+   var value = "";
+
+   for (let i = repo_list.length - 1; i >= 0; i--) {
+      value += `<li class="list-item">
+                                    <div class="row">
+                                       <div class="col-lg-9">
+                                          <div class="top">
+                                             <h3>${repo_list[i].name}</h3>
+                                          </div>
+                                          
+                                          <div style ="display : ${
+                                             repo_list[i].description === null
+                                                ? "none"
+                                                : "block"
+                                          };   margin: 10px auto 5px;
+                                             color: #586069;"> ${
+                                                repo_list[i].description
+                                             } </div>
+                                          <div class="bottom">
+                                             <span class="lang" style="display : ${
+                                                repo_list[i].languages.nodes[0]
+                                                   ?.color
+                                                   ? "inline-block"
+                                                   : "none"
+                                             }">
+                                                <span
+                                                   class="color"
+                                                   style="
+                                                      background-color: ${
+                                                         repo_list[i].languages
+                                                            .nodes[0]?.color
+                                                      };
+                                                   "
+                                                ></span>
+                                                <span class="text"
+                                                   >${
+                                                      repo_list[i].languages
+                                                         .nodes[0]?.name
+                                                   }</span
+                                                >
+                                             </span>
+                                             <span class="star" style="display : ${
+                                                repo_list[i].stargazerCount > 0
+                                                   ? "inline-block"
+                                                   : "none"
+                                             }">
+                                                <i class="far fa-star"></i>
+                                                ${repo_list[i].stargazerCount}
+                                             </span>
+                                             <span class="fork" style="display : ${
+                                                repo_list[i].forkCount > 0
+                                                   ? "inline-block"
+                                                   : "none"
+                                             }">
+                                                <i
+                                                   class="fas fa-code-branch"
+                                                ></i>
+                                                ${repo_list[i].forkCount}
+                                             </span>
+                                             <span class="time">
+                                                Updated on <span>${new Date(
+                                                   repo_list[i].updatedAt
+                                                ).toLocaleDateString(
+                                                   "en-EN",
+                                                   options
+                                                )}</span>
+                                             </span>
+                                          </div>
+                                       </div>
+
+                                       <div class="col-lg-3">
+                                          <div class="star-btn">
+                                             <button class="btn">
+                                                <i class="far fa-star"></i> Star
+                                             </button>
+                                          </div>
+                                       </div>
+                                    </div>
+                                 </li>`;
+   }
+   repo.innerHTML = value;
+}
